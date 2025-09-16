8e9f9c6080a3138d079431454c342e42f69a598c
Thomas Rustemeyer <thomas.rustemeyer@me.com>
2025-09-17 00:56:42 +0200

    Table development

--- render-table.go
+++ render-table.go
@@ -1,10 +1,12 @@
 package zeichenwerk
 
-func (r *Renderer) renderTableHeader(table *Table, x, y, w, h int) {
+var emptyBorder = BorderStyle{InnerH: ' ', InnerV: '-'}
+
+func (r *Renderer) renderTableHeader(table *Table, x, y, w, h int, grid bool, border BorderStyle) {
 	rx := 0 // current render position
 	rw := w // remaining width
-	for _, column := range table.provider.Columns() {
-		table.Log(table, "debug", "Render header %s %d %d", column.Header, rx, rw)
+	columns := table.provider.Columns()
+	for i, column := range columns {
 		// Check, if the column is visible
 		if rx+column.Width < table.offsetX {
 			rx = rx + column.Width + 1
@@ -18,14 +20,29 @@ func (r *Renderer) renderTableHeader(table *Table, x, y, w, h int) {
 			rw = rw - rcw - 1
 		} else {
 			r.text(x-table.offsetX+rx, y, column.Header, column.Width)
+			r.repeat(x-table.offsetX+rx, y+1, 1, 0, column.Width, border.InnerH)
+			if i < len(columns)-1 && grid {
+				r.screen.SetContent(x-table.offsetX+rx+column.Width, y+1, border.InnerTopT, nil, r.style)
+				r.screen.SetContent(x-table.offsetX+rx+column.Width, y+h, border.BottomT, nil, r.style)
+			}
 			rw = rw - column.Width - 1
 		}
 		rx = rx + column.Width + 1
+	}
+
+	// Draw the remaining header line
+	if rx-table.offsetX < w {
+		r.repeat(x-table.offsetX+rx-1, y+1, 1, 0, w+table.offsetX-rx+1, border.InnerH)
+	}
+	if grid {
+		r.screen.SetContent(x-1, y+1, border.LeftT, nil, r.style)
+		r.screen.SetContent(x+w, y+1, border.RightT, nil, r.style)
 	}
 }
 
 func (r *Renderer) renderTableContent(table *Table, x, y, w, h int, separator rune) {
 	row := table.offsetY
+	columns := table.provider.Columns()
 	for row < table.provider.Length() && row-table.offsetY < h {
 		rx := 0
 		if row == table.row && table.focused {
@@ -35,7 +52,7 @@ func (r *Renderer) renderTableContent(table *Table, x, y, w, h int, separator ru
 		} else {
 			r.SetStyle(table.Style(""))
 		}
-		for i, column := range table.provider.Columns() {
+		for i, column := range columns {
 			if rx+column.Width < table.offsetX {
 				rx = rx + column.Width + 1
 				continue
@@ -43,7 +60,9 @@ func (r *Renderer) renderTableContent(table *Table, x, y, w, h int, separator ru
 			if rx < table.offsetX {
 			} else {
 				r.text(x-table.offsetX+rx, y-table.offsetY+row, table.provider.Str(row, i), column.Width)
-				r.screen.SetContent(x-table.offsetX+rx+column.Width, y-table.offsetY+row, separator, nil, r.style)
+				if i < len(columns)-1 {
+					r.screen.SetContent(x-table.offsetX+rx+column.Width, y-table.offsetY+row, separator, nil, r.style)
+				}
 			}
 			rx = rx + column.Width + 1
 		}
@@ -56,12 +75,10 @@ func (r *Renderer) renderTable(table *Table) {
 	style := table.Style("")
 	if style.Border != "" {
 		border := r.theme.Border(style.Border)
-		r.renderTableHeader(table, x, y, w, 1)
-		r.line(x-1, y+1, 1, 0, w, border.LeftT, border.InnerH, border.RightT)
+		r.renderTableHeader(table, x, y, w, h, true, border)
 		r.renderTableContent(table, x, y+2, w, h-2, border.InnerV)
 	} else {
-		r.renderTableHeader(table, x, y, w, 1)
-		r.line(x, y+1, 1, 0, w-2, '-', '-', '-')
+		r.renderTableHeader(table, x, y, w, h, false, emptyBorder)
 		r.renderTableContent(table, x, y+2, w, h-2, ' ')
 	}
 }
--- table.go
+++ table.go
@@ -36,13 +36,35 @@ func (t *Table) Handle(evt tcell.Event) bool {
 
 	switch event.Key() {
 	case tcell.KeyDown:
-	  if t.row < t.provider.Length - 1 {
+		if t.row < t.provider.Length()-1 {
 			t.row++
+			t.adjust()
 		}
-		t.adjust()
-	case tcell.
+		return true
+	case tcell.KeyUp:
+		if t.row > 0 {
+			t.row--
+			t.adjust()
+		}
+		return true
 	default:
 		return false
 	}
 }
 
+func (t *Table) adjust() {
+	// Get actual content height
+	_, h := t.Size()
+
+	// We do not need to adjust anything, if all rows fit
+	if t.provider.Length() < h-2 {
+		return
+	}
+	if t.row < t.offsetY {
+		t.offsetY = t.row
+	}
+	if t.row > t.offsetY+h-3 {
+		t.offsetY = t.row - h + 3
+	}
+	t.Refresh()
+}
